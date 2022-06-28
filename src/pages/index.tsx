545b52c511a7d6e4e7906cc077d4afcea330606f
waynew99 <waynew99@users.noreply.github.com>
2022-06-28 19:26:40 +0000

    Deploying to gh-pages from @ waynew99/econsimstest@502c25fad655cf47ddb44df56183875eb759297b 🚀

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import SimulationMenu from '../components/SimulationMenu';
-
-export default function Home() {
-  return (
-    <div className="wrapper">
-      <SimulationMenu />
-    </div>
-  )
-}
